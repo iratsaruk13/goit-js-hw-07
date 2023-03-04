d3509bd6685826b73a542c1977cf69dc403f35f2
iratsaruk13 <115734138+iratsaruk13@users.noreply.github.com>
2023-03-04 21:47:19 +0200

    make 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// ------------Функція створення розмітки елемента галереї
+
+function createGalleryItems (items) {
+return items.map(({preview, original, description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+}).join('');
+}
+
+
+// -----------Реалізація делегування та отримання url 
+const galleryEl = document.querySelector(".gallery")
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryItems(galleryItems));
+console.log(galleryEl);
+
+galleryEl.addEventListener('click', onGalleryImageClick)
+
+function onGalleryImageClick (evt) {
+    evt.preventDefault();
+    if (!evt.target.classList.contains("gallery__image")) {
+        return;
+    }
+    // console.log(evt.target);
+    const galleryImageUrl = evt.target.dataset.source;
+    console.log(galleryImageUrl);
+    makeModalWindow(galleryImageUrl);
+}
+
+// -----------Відкриття модального вікна по кліку на елементі галереї
+
+function makeModalWindow (url) {
+    window.addEventListener('keydown', onKeyDownEsc);
+    const modalWindow = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+modalWindow.show();
+function onKeyDownEsc(evt) {
+    if (evt.code === "Escape") {
+      modalWindow.close();
+    }
+    window.removeEventListener("keydown", onKeyDownEsc);
+  }
+}
